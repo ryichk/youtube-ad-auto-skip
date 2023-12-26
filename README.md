2a92b1bc1e842e9fe48a804f83c9330ae61a94ae
ryichk <ryichk@gmail.com>
2023-12-27 04:48:15 +0900

    :bug: Fix a bug that regular videos were also skipped after skipping ads

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## Description
 
 This is a Chrome extension that automatically skips Youtube ads.
+
+Just install this extension and it will automatically skip ads within 1 second.
+
+No side effects.
--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,23 @@
+const sleep = (time) => {
+  return new Promise(resolve => setTimeout(resolve, time));
+};
 
-const observer = new MutationObserver((mutations) => {
+const observer = new MutationObserver(async (mutations) => {
   for (const mutation of mutations) {
-    // 広告がある場合のみ処理を実行
+    // 広告がある場合のみ処理を実行します
     if (mutation.addedNodes.length &&
         mutation.addedNodes[0].parentNode &&
         mutation.addedNodes[0].parentNode.className == 'ytp-ad-text'
     ) {
+      // 広告スキップ後に通常の動画もスキップしてしまう時があるので待ち時間を設けます
+      await sleep(100);
+
+      // 再生中の動画が広告なのか判断し、広告の場合のみ後続の処理を続行させます
+      const videoAdsElements = document.getElementsByClassName('video-ads');
+      if (!videoAdsElements[0].children) {
+        return;
+      }
+
       const videoElements = document.getElementsByTagName('video');
       if (videoElements.length) {
         videoElements[0].currentTime = 999;
