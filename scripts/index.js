47e5f668984abc825a6abbedbe55f165ef021ca8
ryichk <ryichk@gmail.com>
2023-12-27 07:17:30 +0900

    :sparkles: Cut out the process of skipping ads as a method.

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,23 @@ const sleep = (time) => {
   return new Promise(resolve => setTimeout(resolve, time));
 };
 
+const skipAd = () => {
+  // 再生中の動画が広告なのか判断し、広告の場合のみ後続の処理を続行させます
+  const videoAdsElements = document.getElementsByClassName('ytp-ad-player-overlay');
+  if (!videoAdsElements.length) {
+    return;
+  }
+
+  const videoElements = document.getElementsByTagName('video');
+  if (videoElements.length) {
+    videoElements[0].currentTime = 999;
+  }
+  const clickButton = document.querySelector('.ytp-ad-skip-button-text');
+  if (clickButton) {
+    clickButton.click();
+  }
+}
+
 const observer = new MutationObserver(async (mutations) => {
   for (const mutation of mutations) {
     // 広告がある場合のみ処理を実行します
@@ -11,21 +28,7 @@ const observer = new MutationObserver(async (mutations) => {
     ) {
       // 広告スキップ後に通常の動画もスキップしてしまう時があるので待ち時間を設けます
       await sleep(100);
-
-      // 再生中の動画が広告なのか判断し、広告の場合のみ後続の処理を続行させます
-      const videoAdsElements = document.getElementsByClassName('video-ads');
-      if (!videoAdsElements[0].children) {
-        return;
-      }
-
-      const videoElements = document.getElementsByTagName('video');
-      if (videoElements.length) {
-        videoElements[0].currentTime = 999;
-      }
-      const clickButton = document.querySelector('.ytp-ad-skip-button-text');
-      if (clickButton) {
-        clickButton.click();
-      }
+      skipAd();
     }
   }
 });
